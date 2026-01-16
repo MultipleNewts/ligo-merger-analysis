5a58e1c57385baf080f397934ffad0099155d5ba
Matt <123matt.hew654@gmail.com>
2026-01-16 15:20:08 +0000

    Add code comments
    Add code comments for white noise class for clarity

--- analysis-techniques/Noise Masks.py
+++ analysis-techniques/Noise Masks.py
@@ -9,16 +9,21 @@ time_seed = time.time_ns()
 rnd_object = np.random.default_rng(seed=time_seed)
 
 
+# Define White Noise Class
 class WhiteNoise:
-    def __init__(self, num, mean, std):
+    # Define required attributes
+    def __init__(self, num, mean, std, samplerate=16000):
         self.label = "White Noise"
         self.mean = mean
         self.std = std
+        self.samplerate = samplerate
         self.gen_noise(num)
 
+    # Generate White Noise
     def gen_noise(self, num):
         self.array = rnd_object.normal(self.mean, self.std, num)
 
+    # Plots Noise Data
     def plot_raw(self):
         plt.plot(self.array)
         plt.axhline(self.mean, color="r", linestyle="--", alpha=0.5, label=r"$\mu$")
@@ -30,6 +35,7 @@ class WhiteNoise:
         plt.legend(loc="upper right")
         plt.show()
 
+    # Plots Distribution of Noise Data
     def plot_hist(self):
         plt.hist(self.array, color="deepskyblue", density=True)
         plt.xlabel("Value")
@@ -37,8 +43,9 @@ class WhiteNoise:
         plt.title(self.label + " Distribution")
         plt.show()
 
+    # Plots Spectrogram of Data
     def plot_spect(self):
-        plt.specgram(self.array, cmap="plasma")
+        plt.specgram(self.array, Fs=self.samplerate, cmap="plasma")
         plt.xlabel("Value")
         plt.ylabel("Proportion")
         plt.title(self.label + " Distribution")
@@ -58,5 +65,12 @@ class WhiteNoise:
         self.std = std
         self.gen_noise(len(self.array))
 
+    def get_len(self):
+        return len(self.array)
+
+    def get_samplerate(self):
+        return self.samplerate
+
+    # Access Noise Data
     def get_noise(self):
         return self.array
