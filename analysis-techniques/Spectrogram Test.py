cae5e87531e918da70ed7222c4c5ae4f5fc81ba7
Matt <123matt.hew654@gmail.com>
2026-01-16 13:34:47 +0000

    Fix styling
    Fix code styling to abide by PEP8

--- analysis-techniques/Spectrogram Test.py
+++ analysis-techniques/Spectrogram Test.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-path = Path(__file__).parent/ "./Hello World.wav"
+path = Path(__file__).parent / "./Hello World.wav"
 
 samplerate, sample = scipy.io.wavfile.read(path)
 sample = sample[30000:90000]
