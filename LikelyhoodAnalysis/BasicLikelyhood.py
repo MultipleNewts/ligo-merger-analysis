0fb1ef2edda4a70bf7649a6eda0eacfe81f42047
BenjaminWigley <ben@wigley.org.uk>
2026-01-16 13:09:54 +0000

    Update BasicLikelyhood.py

--- LikelyhoodAnalysis/BasicLikelyhood.py
+++ LikelyhoodAnalysis/BasicLikelyhood.py
@@ -1,51 +1,65 @@
 # %%
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 import time
 
 
-# Config Random
+# Configure Random
 time_seed = time.time_ns()
 rnd_object = np.random.default_rng(seed=time_seed)
 
-
-# Define parameters
-N = 441000
+# Define Parameters
 t_start = 0
-t_end = 10
+t_end = 200
+sample_rate = 300
+N = np.int32((t_end-t_start) * sample_rate)
 
 noise_mean = 0
 noise_std = 1
 
+signal_wavenumber = 2 * np.pi * 100
+signal_width = 2
+signal_amplitude = 1
 
-# Generate Noise
+
+# Signal Model
+def model(t, t0=0):
+    return (signal_amplitude *
+            np.exp((-np.power((t-t0)/signal_width, 2))) *
+            np.sin((t-t0)*signal_wavenumber))
+
+
+# Generate Data
 times = np.linspace(t_start, t_end, N, endpoint=True)
 noise = rnd_object.normal(noise_mean, noise_std, N)
 
+t0 = rnd_object.uniform(t_start, t_end)
+signal = model(times, t0)
+
+data = signal + noise
 
 # %%
-plt.plot(times, noise)
-plt.ylabel("Displacement")
+# Display
+plt.plot(times, data, label="data")
+plt.plot(times, signal, label="signal")
+plt.legend(loc="upper right")
+plt.ylabel("Amplitude")
 plt.xlabel("Time")
-plt.title("White noise")
+plt.title("Basic Model")
+plt.show()
+
+# %%
+log_likelihood = np.zeros(N)
+for i in range(0, N):
+    log_likelihood[i] = -np.sum(np.power((data-model(times, times[i])), 2))
+    # log_likelihood[i] = -np.sum(np.power((noise), 2))
+
+MLE = np.argmax(log_likelihood)
+
+plt.plot(times, log_likelihood, label="Log-Likelihood")
+plt.axvline(MLE/N*t_end, c="red", linestyle="--", alpha=0.7, label="MLE")
+plt.legend()
 plt.show()
 
 
 # %%
-# Show Periodogram
-
-freq_bins = np.fft.fftfreq(N, (t_end-t_start)/N)
-noise_transform = np.fft.fft(noise)
-
-noise_power = 10 * np.log10(np.power(np.abs(noise_transform), 2))
-
-plt.bar(freq_bins, noise_power)
-plt.xscale("log")
-
-plt.xlim((0, np.max(freq_bins)))
-plt.xlabel("Frequency / Hz")
-plt.ylabel("Spectral Power / dB/Hz")
-plt.title("White noise Periodogram")#
-plt.show()
-# %%
-# Attempt as a filter
