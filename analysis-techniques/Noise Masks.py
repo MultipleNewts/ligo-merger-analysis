401bfcb60cbe7f6d040cdd8adee07c54e99aef09
Matt <123matt.hew654@gmail.com>
2026-01-16 14:19:56 +0000

    Add visualisations
    Add visualisations for white noise, includes: spectrogram, histogram, and raw output

--- analysis-techniques/Noise Masks.py
+++ analysis-techniques/Noise Masks.py
@@ -1,6 +1,6 @@
 # %%
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import time
 
 
@@ -11,12 +11,38 @@ rnd_object = np.random.default_rng(seed=time_seed)
 
 class WhiteNoise:
     def __init__(self, num, mean, std):
+        self.label = "White Noise"
         self.mean = mean
         self.std = std
         self.gen_noise(num)
 
     def gen_noise(self, num):
         self.array = rnd_object.normal(self.mean, self.std, num)
+
+    def plot_raw(self):
+        plt.plot(self.array)
+        plt.axhline(self.mean, color="r", linestyle="--", alpha=0.5, label=r"$\mu$")
+        plt.axhline(self.mean+self.std, color="k", linestyle="--", alpha=0.5, label=r"$1\sigma$")
+        plt.axhline(self.mean-self.std, color="k", linestyle="--", alpha=0.5)
+        plt.xlabel("Datapoint")
+        plt.ylabel("Value")
+        plt.title(self.label)
+        plt.legend(loc="upper right")
+        plt.show()
+
+    def plot_hist(self):
+        plt.hist(self.array, color="deepskyblue", density=True)
+        plt.xlabel("Value")
+        plt.ylabel("Proportion")
+        plt.title(self.label + " Distribution")
+        plt.show()
+
+    def plot_spect(self):
+        plt.specgram(self.array, cmap="plasma")
+        plt.xlabel("Value")
+        plt.ylabel("Proportion")
+        plt.title(self.label + " Distribution")
+        plt.show()
 
     def get_mean(self):
         return self.mean
