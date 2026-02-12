662f710dfd77e5a4f27d9451f37070eaf307da19
Matt <123matt.hew654@gmail.com>
2026-02-12 12:05:53 +0000

    Add segment length benchmark
    Add ability to benchmark effectiveness of segment length in Welch's method

--- analysis_techniques/welch benchmarking.py
+++ analysis_techniques/welch benchmarking.py
@@ -45,27 +45,27 @@ plt.tight_layout()
 plt.show()
 
 # %%
-N = 20
-seglengths = np.arange(N, step=2)*4096
+seglengths = np.array([5, 10, 15])*4096
 seg_data = []
 runtime = []
 for seg in seglengths:
+    print(seg)
     temp = time.time_ns()
     seg_data.append(run_welch(seg, 0.5))
     runtime.append(time.time_ns()-temp)
 
-fig, ax = plt.subplots(2, 1)
-for overlap, dataset in zip(overlaps, overlap_data):
-    ax[0].plot(*dataset, label=f"{overlap} overlap")
-ax[1].plot(seglengths, runtime, label="Time Taken")
-
-ax[0].loglog()
-ax[0].set_xlabel(r"Frequency/$Hz$")
-ax[0].set_ylabel(r"Relative Power")
-ax[0].set_title("PSD from Welch's Method")
-ax[1].set_xlabel(r"Overlap Ratio")
-ax[1].set_ylabel(r"Time Taken/$ns$")
-ax[1].set_title("PSD from Welch's Method")
-ax[1].legend()
+fig, ax = plt.subplots(4, 1, figsize=(4, 15))
+for i, seg, dataset in zip(np.arange(3), seglengths, seg_data):
+    ax[i].plot(*dataset, label=f"{seg} Datapoints")
+    ax[i].loglog()
+    ax[i].set_xlabel(r"Frequency/$Hz$")
+    ax[i].set_ylabel(r"Relative Power")
+    ax[i].set_title("PSD from Welch's Method")
+ax[3].plot(seglengths, runtime, label="Time Taken")
+ax[3].set_xlabel(r"Segment Length")
+ax[3].set_ylabel(r"Time Taken/$ns$")
+ax[3].set_title("PSD from Welch's Method")
+ax[3].legend()
 plt.tight_layout()
 plt.show()
+
