d2d786631a0231b3d9575ba1098c3dd3a4927a9c
Matt <123matt.hew654@gmail.com>
2026-03-05 17:48:46 +0000

    Implement event detector module
    Implement module to scan large window of data and determine if events are present at any point in dataset. WIP seems to have many false positives

---
+++ analysis_techniques/event_detector.py
@@ -0,0 +1,88 @@
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal.windows import tukey
+from analysis_techniques.ligonoise import LIGOEvent
+from analysis_techniques.welch_method import welch
+from analysis_techniques.data_processing import whiten, bandpass
+from analysis_techniques.templateImporting import Templates
+
+
+# %%
+def process_data(data, fs, freqs, PSD):
+    whitened_data = whiten(data, fs, freqs, PSD, tukey)
+    bp_data = bandpass(whitened_data, fs, order=8)
+    return bp_data
+
+
+# %%
+def detect_events(full_data, template_bank, fs=4096, window_func=tukey, seg_dur=4, overlap=0.75):
+    # compute Welch PSD
+    freqs, PSD = welch(strain, fs, window_func, int(seg_dur*fs), overlap=overlap)
+    # compute test length
+    test_length = len(template_bank.get_template(0).hp)
+    if test_length % 2 != 0:
+        test_length -= 1
+    test_index = test_length - 1
+    # moving window setup
+    overlap = 0.5
+    step = int(test_length*(1 - overlap))
+    segments = []
+    processed = []
+    events = []
+
+    # segments full dataset into segments
+    max_iter = int((len(full_data) - test_length)//step) + 1
+    for i in range(max_iter):
+        index = int(i*step)
+        segments.append(full_data[index:(index+test_length)])
+
+    # test each window
+    for i in range(template_bank.template_count):
+        print("reached")
+        template = template_bank.get_template(i)
+        model = template.hp[:test_index]
+        proc_model = process_data(model, fs, freqs, PSD)
+        model_times = template.times[:test_index]
+        for j, segment in enumerate(segments):
+            if j % 100 == 0:
+                print(f"Processing Segment {j}...")
+            proc_data = process_data(segment, fs, freqs, PSD)
+            test_dur = test_length/fs
+            dt = 1/fs
+            times = np.linspace(0, test_dur, segment.shape[0])
+            temp_data = np.zeros(test_length)
+            for k in range(test_length):
+                interp_data = np.interp(model_times, times+(dt*k), proc_data)
+                inner_product = np.sum(interp_data[:-1] * proc_model)
+                temp_data[k] = inner_product
+                if inner_product > 600:
+                    events.append(j*test_dur + k*dt)
+                    processed.append(
+                        (np.linspace(
+                                    (j*test_dur),
+                                    (j*test_dur + k*test_length),
+                                    test_length,
+                                    endpoint=True
+                                    ),
+                         temp_data))
+
+    return events, processed
+
+
+# %%
+# if __name__ == "__main__":
+EventObject = LIGOEvent(200, 7)
+strain = EventObject.get_data()
+t0, dt = EventObject.get_time_vars()
+fs = 1/dt.value
+Template_Manager = Templates("templates/Event7Template.json")
+template = Template_Manager.get_template(0)
+# %%
+event_times, ip_vals = detect_events(strain, Template_Manager, fs, tukey, 4, 0.5)
+
+# %%
+print(len(event_times))
+print(event_times)
+plt.plot(*ip_vals[200])
+# %%
