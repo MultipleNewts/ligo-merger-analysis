63f2149f4c89b6f8dc92fa671c8e6006d030fbb5
BenjaminWigley <ben@wigley.org.uk>
2026-03-17 12:12:50 +0000

    Save processed data to json
    Data takes ~45 mins to process. To save time recompiling, data will be saved and able to be reimported to execute different variants of find_best_data_v2()

--- analysis_techniques/final_detection_v2.py
+++ analysis_techniques/final_detection_v2.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from analysis_techniques.welch_method import welch
 from analysis_techniques.data_processing import whiten, bandpass
+import json
 
 # %%
 EO3 = LIGOEvent(200, 3)
@@ -16,7 +17,7 @@ strain = EO3.get_data()
 t0, dt = EO3.get_time_vars()
 fs = 1/dt.value
 
-Template_Manager = Templates("templates/Event3HigherRes.json")
+Template_Manager = Templates("templates/Event7LowResHighRange.json")
 
 # %%
 event_times, event_plots, products = detect_events_v2(strain, Template_Manager, fs, tukey, 4, 0.5)
@@ -24,3 +25,7 @@ event_times, event_plots, products = detect_events_v2(strain, Template_Manager, 
 # %%
 find_best_data_v2(Template_Manager, event_times, event_plots)
 # %%
+data_to_save = {"data": [event_times, event_plots, products]}
+myJson = json.dumps(data_to_save)
+with open("Results/processed_templates/Event7LowResHighRangeProcessed.json", "w") as f:
+    f.write(myJson)
